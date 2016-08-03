17e04d71f970905c6213ac26053039e88ee41b53
fscarpin <fernando.scarpin@gmail.com>
2016-08-03 14:47:59 -0300

    Setup Artifact to upload files to AWS S3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,4 +63,8 @@ module ApplicationHelper
   def tenant_plan(tenant_id)
     return Tenant.find(tenant_id).plan
   end
+
+  def s3_link(tenant_id, artifact_key)
+    link_to(artifact_key, "#{artifact_key}", class: "main-link", target: "new")
+  end
 end
