822fcab6c6a4d466294b05f4170041dfbd0ecd69
fscarpin <fernando.scarpin@gmail.com>
2016-08-02 22:37:40 -0300

    1. Added upload_file_to_amazon_s3 to Artifact model, and also a before_save calling this method, so the file can be uploaded when save method is called 2. Set up bootstrap theme for artifacts

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,16 +1,26 @@
 class Artifact < ActiveRecord::Base
   MAX_FILE_SIZE = 10.megabytes
 
+  before_save :upload_file_to_amazon_s3
+
   attr_accessor :file
   belongs_to :project
   validates_uniqueness_of :name
   validates_presence_of :name, :file
 
-  validate :uploaded_file_size
+  validate :max_upload_file_size
 
   private
 
-  def uploaded_file_size
+  def upload_file_to_amazon_s3
+    s3 = Aws::S3::Resource.new
+    tenant_name = self.project.tenant.name
+    obj = s3.bucket(ENV['AWS_S3_BUCKET_PM_APP']).object(tenant_name + "/" + file.original_filename)
+    obj.upload_file(file.path, acl: "public-read")
+    self.key = obj.public_url
+  end
+
+  def max_upload_file_size
     if file && file.size > MAX_FILE_SIZE
       errors.add(:file, "File size must be less than #{MAX_FILE_SIZE}")
     end
